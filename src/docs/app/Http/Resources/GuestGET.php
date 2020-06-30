ec2b5841c11ddcd79895915132c8bde477d3232e
wojciechgula <gulasz101@gmail.com>
2020-06-30 15:14:14 +0200

    Updating docs.

--- src/docs/app/Http/Resources/GuestGET.php
+++ src/docs/app/Http/Resources/GuestGET.php
@@ -3,11 +3,11 @@
 /**
  * @OA\Schema(schema="GuestGETAttributes", required={ "id_number", "first_name", "last_name", "email", "phone", "city", "country", "created_at", "updated_at" },
  *
- *  @OA\Property(property="id_number", type="string", minLength=6, maxLength=30, example="ABS123456"),
- *  @OA\Property(property="first_name", type="string", minLength=3, maxLength=100, example="John"),
- *  @OA\Property(property="last_name", type="string", minLength=3, maxLength=100, example="Doe"),
- *  @OA\Property(property="email", type="string", maxLength=100, example="john.doe@example.com"),
- *  @OA\Property(property="phone", type="string", minLength=9, maxLength=30, example="+48710072772"),
+ *  @OA\Property(property="id_number", type="string", maxLength=30, example="ABS123456"),
+ *  @OA\Property(property="first_name", type="string", maxLength=100, example="John"),
+ *  @OA\Property(property="last_name", type="string", maxLength=100, example="Doe"),
+ *  @OA\Property(property="email", type="string", minLength=3, maxLength=100, example="john.doe@example.com"),
+ *  @OA\Property(property="phone", type="string", maxLength=30, example="+48710072772"),
  *  @OA\Property(property="country", minLength=2, maxLength=2, example="PL"),
  *  @OA\Property(property="created_at", type="string", format="Y-m-d H:i:s", example="2020-01-01 11:55:00"),
  *  @OA\Property(property="updated_at", type="string", format="Y-m-d H:i:s", example="2020-01-01 11:55:00"),
